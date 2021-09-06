f2abc6ef8a9926f37dfd5da9ddea84837297d3fa
evren <evrenuzuntas@gmail.com>
2021-09-06 10:18:58 +0300

    console.log(inputs)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
@@ -28,9 +28,9 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 export default App;
